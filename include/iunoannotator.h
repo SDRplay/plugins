4ceabe045c663aa848e7dd4cfb81a71e03f8554b
SDRplay <SDRplay@users.noreply.github.com>
2020-07-08 12:04:14 +0100

    Added MarkerAndLine Style

--- include/iunoannotator.h
+++ include/iunoannotator.h
@@ -7,7 +7,8 @@ typedef enum
 {
 	AnnotatorStyleFlag = 1,
 	AnnotatorStyleBox = 2,
-	AnnotatorStyleMarker = 3
+	AnnotatorStyleMarker = 3,
+	AnnotatorStyleMarkerAndLine = 4
 } IUnoAnnotatorStyle;
 
 typedef struct
